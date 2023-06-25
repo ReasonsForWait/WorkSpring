1833faf679cce7e230c4f3eee8b680910dcc5275
ReasonsForWait <leekjgo21@naver.com>
2023-06-25 20:15:53 +0900

    login 구현해보기

---
+++ loginsample1/src/main/java/com/example/loginsample1/Loginsample1Application.java
@@ -0,0 +1,13 @@
+package com.example.loginsample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Loginsample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Loginsample1Application.class, args);
+	}
+
+}
---
+++ loginsample1/src/test/java/com/example/loginsample1/Loginsample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginsample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Loginsample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
